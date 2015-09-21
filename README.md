a6904ac9118863af0114b7766c2644cce6da9c7b
itkatun <itkatun@gmail.com>
2015-09-21 17:31:25 +0600

    подготовка проекта

--- README.md
+++ README.md
@@ -1,27 +1,30 @@
+# loftschool-shop
 <h1>Как установить мой проект</h1>
 
 <ul>
 <li>1. git clone https://github.com/itkatun/shop.git site</li>
 <li>2. cd site</li>
-<li>3. bower i jquery normalize.css modernizer -S</li>
-<li>4. bower i bpopup -S</li>
+<li>3. npm install</li>
+<li>4. bower install</li>
 <li>5. npm i gulp browser-sync -D</li>
 <li>npm install jade --global</li>
+<li>npm install sass --global</li>
+
 <li>6. gulp</li>
 </ul>
 
 <p>
 --------------------------------------------------------
 </br>
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin
-git push -u origin master
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git remote add origin
+# git push -u origin master
 
 
-
-git remote add origin https://github.com/itkatun/shop.git
-git push -u origin master
+##
+# git remote add origin https://github.com/itkatun/template.git
+# git push -u origin master
 --------------------------------------------------------
 </p>
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,94 @@
-var gulp = require("gulp"),
-	browserSync = require('browser-sync');
+'use strict';
 
-gulp.task('server',function (){
-	browserSync ({
-		port:9000,
-		server: {
-			baseDir: 'app'
-		}
+var
+	gulp = require('gulp'),
+	concatCss = require('gulp-concat-css'),
+	minifyCSS = require('gulp-minify-css'),
+	notify = require("gulp-notify"),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	rename = require("gulp-rename"),
+	merge = require('merge-stream'),
+	connect = require('gulp-connect'),
+	prettify = require('gulp-html-prettify'),
+	jsmin = require('gulp-jsmin'),
+	del = require('del');
+
+gulp.task('connect', function() {
+	connect.server({
+		root: ['html', 'css'],
+		livereload: true
 	});
 });
 
-gulp.task ('watch', function () {
-	gulp.watch ([
-		'app/*.html',
-		'app/js/**/*.js',
-		'app/css/**/*.css'
-		]).on('change', browserSync.reload);
-		});
-	
-gulp.task('default', ['server','watch']);
+gulp.task('concat', function () {
+	gulp.src('./_dev/_styles/**/*.css')
+		.pipe(concatCss("main.css"))
+		.pipe(minifyCSS({keepBreaks:true}))
+		.pipe(gulp.dest('css/'))
+		.pipe(notify("Watch Complete!"));
+		//.pipe(connect.reload());
+});
+
+gulp.task('jade', function () {
+	gulp.src('./_dev/_makeups/_pages/*.html')
+		.pipe(prettify({indent_char: '  ', indent_size: 2}))
+		.pipe(gulp.dest('html/'))
+		.pipe(notify("Jade Complete!"));
+		//.pipe(connect.reload());
+});
+
+gulp.task('compress-plugins', function() {
+	gulp.src('_dev/_scripts/_plugins/*.js')
+		.pipe(concat('plugins.js'))
+		.pipe(gulp.dest('js/_source/'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('js/'))
+		.pipe(notify("Plugins.js Optimized!"));
+});
+
+
+gulp.task('compress-modules', function() {
+	gulp.src('_dev/_scripts/_modules/*.js')
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('js/_source/'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(jsmin())
+		.pipe(gulp.dest('js/'))
+		.pipe(notify("modules.js Optimized!"));
+});
+
+gulp.task('clean', function() {
+	del(['_dev/_makeups/**/*.html', '_dev/_styles/**/*.css', '_dev/_bower']);
+});
+
+gulp.task('clean-total', function() {
+	del(['_dev', 'node_modules', '.bowerrc', '.gitignore', 'gulpfile.js', 'package.json']);
+});
+
+//gulp.task('scripts', function() {
+//
+//	var main = gulp.src('./_dev/_scripts/_modules/*.js');
+//	var plugins = gulp.src('./_dev/_scripts/_plugins/*.js');
+//
+//	return merge(main)
+//		.pipe(concat('project.js'))
+//		.pipe(gulp.dest('/js/_source/'))
+//		.pipe(rename('project.min.js'))
+//		.pipe(uglify())
+//		.pipe(gulp.dest('/js/'))
+//});
+
+
+gulp.task('watch', function(){
+	gulp.watch('_dev/_styles/**/*.css', ['concat']);
+	gulp.watch('_dev/_makeups/**/*.html', ['jade']);
+	gulp.watch('_dev/_scripts/_plugins/*.js', ['compress-plugins']);
+	gulp.watch('_dev/_scripts/_modules/*.js', ['compress-modules']);
+});
+
+gulp.task('default', ['watch']);
+
+
+
